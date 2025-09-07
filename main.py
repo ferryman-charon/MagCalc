02e85580d15fbbf8190dcb5d0f4f244a06eb6c45
ferryman <ferrymancharon@gmx.at>
2025-09-07 22:01:01 +0200

    test

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello")
+import numpy as np
+import matplotlib.pyplot as plt 
+
+N = 50
+dim = (N, N)
+
+init_rand = np.random.random(dim)
+lattice = np.zeros(dim)
+lattice[init_rand>=0.75] = 1
+lattice[init_rand<0.75] = -1
+
+plt.imshow(lattice)
+plt.show()
